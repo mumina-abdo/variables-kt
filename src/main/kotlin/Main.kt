d0da3d00cf8ffaee8d6572fc91cc88bd73bfd814
Mumina-Abdo <muminaboruabdo@gmail.com>
2024-02-07 23:38:16 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+ val studentName = "sadia hassan"
+ var studentAge = "21"
+  var studentNumber = "0769071112"
+   var weight ="68.5F"
+val kenyan = "true"
+
+ println("studentName: sadia hassan")
+ println("studentAge: 21")
+ println("studentNumber: 0769071112")
+ println("weight: 68.5F")
+ println("kenyan: true")
+
+
+}
